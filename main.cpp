49e095b3d3b1d10c0b5f14d07cc782d017c14f43
diwacreation3 <samariweather@gmail.com>
2023-08-06 14:21:16 +0545

    update 🍁

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
     {
         sf::Event event;
         while (window.pollEvent(event))
+        
         {
             if (event.type == sf::Event::Closed)
                 window.close();
