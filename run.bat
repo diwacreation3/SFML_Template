40a553e8f1d183c17587eddfc3dff4fb82e1c45c
diwacreation3 <diwakarcreation@gmail.com>
2023-09-21 19:30:45 +0545

    Build error Fixed 🍀

--- run.bat
+++ run.bat
@@ -1,10 +1,7 @@
-
-echo Building project..
+echo Building project 
 
 g++ -Iheaders/sfml/include -c main.cpp
 
-g++ main.o -o main -Lheaders/sfml/lib  -lsfml-graphics -lsfml-window -lsfml-system -lopengl32 -lsfml-audio
-
-echo Build Complete
+g++ main.o -o main -Lheaders/sfml/lib -lsfml-graphics -lsfml-window -lsfml-system -lopengl32 -lsfml-audio
 
 main
